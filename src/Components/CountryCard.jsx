3eb602d2bda084eaab355396f54b72d2728b3aa9
RitheshJgit <ritheshjayapal@gmail.com>
2025-02-12 22:16:30 +0530

    Update CountryCard.jsx

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -1,12 +1,15 @@
+
 function CountryCard({ name, flag, population, region, capital }) {
   return (
-    <>
-      <h1>{name}</h1>
+    <div className="">
+      <div className="box">
+      <h2>{name}</h2>
       <img src={flag} alt="" />
-      <h3>Population{population}</h3>
+      <p>Population : {population}</p>
       <h3>{region}</h3>
       <h3>{capital}</h3>
-    </>
+      </div>
+    </div>
   );
 }
 
